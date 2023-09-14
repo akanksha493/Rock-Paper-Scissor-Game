fd0fe1b1e00ce378448ccb4ac3ce9bbfaa50f706
akanksha493 <akanksha.ydv.aku@gmail.com>
2023-09-14 22:40:24 +0530

    added ui

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rock Paper Scissor Game
+A javascript project based on traditional [rock paper scissor game](https://en.wikipedia.org/wiki/Rock_paper_scissors). This project is part of [Odin Project](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors) Curriculum.
+
+### How Does It Work?
+Just like the original Rock-Paper-Scissor there are only three rules:
+
+1. Rock beats Scissor.
+2. Scissor beats Paper.
+3. and Paper beats Rocks.
+
+And that it. Thats how it works. Give it a try.
+
+Live Preview : [website-link](https://akanksha493.github.io/Rock-Paper-Scissor-Game/)
+
+Screenshot of website: 
+
+![screenShot](./screenshot.png)
+
+### Useful Resources
+1. For sound effects: [mixkit.co](https://mixkit.co/free-sound-effects/win/), [pixelbay](https://pixabay.com/sound-effects/search/buzzer/).
+2. For font : [google fonts](https://fonts.google.com/).
+3. Other resources :  stack-overflow, w3school, 
+[article on fluid typography](https://web.dev/min-max-clamp/).
--- script.js
+++ script.js
@@ -1,54 +1,157 @@
 // import prompt form 'prompt-sync';
 
 const choice = ["rock","paper","scissor"];
+const resultDiv = document.querySelector("#result-container");
+const computerChoiceDiv = document.querySelector("#computer-choice");
+const playerChoiceDiv = document.querySelector("#player-choice");
+ 
 
 function getComputerChoice(){
     return choice[Math.floor(Math.random()*3)];
 }
 
+function playAudio(audio){
+    audio.currentTime = 0;
+    audio.play();
+    const playAgainBttn = document.querySelector("#play-again");
+    playAgainBttn.addEventListener('click',()=>{
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
+
+
 function play(playerSelection, computerSelection){
+    result = "";
+    score = 0;
+    const imgRock = document.createElement("img");
+    imgRock.src = "images/hand-rock.jpg"; 
+    const imgPaper = document.createElement("img");
+    imgPaper.src = "images/hand-paper.jpg"; 
+    const imgScissor = document.createElement("img");
+    imgScissor.src = "images/hand-scissor.jpg";
+    // parent.removeChild(parent.firstChild);
+    // console.log(computerChoiceDiv.firstChild);
+    if(computerChoiceDiv.firstChild){
+        computerChoiceDiv.removeChild(computerChoiceDiv.firstChild);
+    }
+    if(playerChoiceDiv.firstChild){
+        playerChoiceDiv.removeChild(playerChoiceDiv.firstChild);
+    }
     
-    if(playerSelection.toLowerCase() == "rock"){
+    if(playerSelection == computerSelection){
+        if(playerSelection == "rock"){
+            playerChoiceDiv.appendChild(imgRock);
+            computerChoiceDiv.appendChild(imgRock.cloneNode(true));
+        }else if(playerSelection =="paper"){
+            playerChoiceDiv.appendChild(imgPaper);
+            computerChoiceDiv.appendChild(imgPaper.cloneNode(true));
+        }else{//scissor
+            playerChoiceDiv.appendChild(imgScissor);
+            computerChoiceDiv.appendChild(imgScissor.cloneNode(true));
+        }
+        result = "Draw";
+        score = -1;
+    }
+    else if(playerSelection == "rock"){
+        playerChoiceDiv.appendChild(imgRock);
         if(computerSelection=="scissor"){
-            console.log("You Win! Rock beats Scissor");
-        }
-        else if(computerSelection=="rock"){
-            console.log("It's a Draw")
+            computerChoiceDiv.appendChild(imgScissor);
+            result = "You Won";
+            score = 1;
         }
         else{
-            console.log("You Loose :( Paper beats Rock");
+            computerChoiceDiv.appendChild(imgPaper);
+            result = "You Loose"; 
         }
-    }else if(playerSelection.toLowerCase() == "paper"){
+    }else if(playerSelection == "paper"){
+        playerChoiceDiv.appendChild(imgPaper);
         if(computerSelection=="rock"){
-            console.log("You Win! Rock beats Paper");
-        }
-        else if(computerSelection=="paper"){
-            console.log("It's a Draw")
+            computerChoiceDiv.appendChild(imgRock);
+            result = "You Won";
+            score = 1;
         }
         else{
-            console.log("You Loose :( Scissor beats Paper");
+            computerChoiceDiv.appendChild(imgScissor);
+            result = "You Loose";
         }
     }else{//scissor
+        playerChoiceDiv.appendChild(imgScissor);
         if(computerSelection=="paper"){
-            console.log("You Win! Scissor beats Paper");
-        }
-        else if(computerSelection=="scissor"){
-            console.log("It's a Draw")
+            computerChoiceDiv.appendChild(imgPaper);
+            result = "You Won";
+            score = 1;
         }
         else{
-            console.log("You Loose :( Rock beats Scissor");
+            computerChoiceDiv.appendChild(imgRock);
+            result = "You Loose";
         }
     }
+    document.querySelector("#result").textContent = result;
+    return score;
+    
 }
-
-
-function playRounds(numberOfRounds){
-    for(let i = 0;i<numberOfRounds;i++){
-        const playerSelection = window.prompt("Enter your choice:");
-        const computerSelection = getComputerChoice();
-        play(playerSelection,computerSelection);
+function playRound(){
+    // const playerSelection = window.prompt("Enter your choice:");
+    let playerScoreContainer = document.querySelector("#player-score");
+    let computerScoreContainer = document.querySelector("#computer-score");
+    playerScore = +playerScoreContainer.textContent;
+    computerScore = +computerScoreContainer.textContent;
+    const playerSelection = this.getAttribute('id');
+    const computerSelection = getComputerChoice();
+   
+    score = play(playerSelection,computerSelection);
+    if(score == 1){
+        playerScore+=1;
+        const audio = document.querySelector("#instant-win");
+        playAudio(audio);
     }
+    else if(score==0){
+        computerScore+=1;
+        const audio = document.querySelector("#instant-loss");
+        playAudio(audio);
+    }else{
+        const audio = document.querySelector("#draw");
+        playAudio(audio);
+    }
+    playerScoreContainer.textContent = playerScore;
+    computerScoreContainer.textContent = computerScore;
+
+    // REMEMBER TO CHANGE IT BACK TO 5 WINS 
+    if(playerScore===5 ){
+        document.querySelector("#final-result").textContent = "YOU WIN!";
+        document.querySelector("#finalResult-container").style.visibility = "visible";
+        const audio = document.querySelector("#win-audio");
+        document.querySelector("#play-field").style.pointerEvents ="none";
+        playAudio(audio);
+        const jsConfetti = new JSConfetti()
+        jsConfetti.addConfetti();
+    }
+    if(computerScore===5){
+        document.querySelector("#final-result").textContent = "YOU LOOSE";
+        document.querySelector("#finalResult-container").style.visibility = "visible";
+        const audio = document.querySelector("#loose-audio");
+        playAudio(audio);
+        document.querySelector("#play-field").style.pointerEvents ="none";
+    }
+   
 }
 
-const rounds =5;
-playRounds(5);
+
+const bttns = document.querySelectorAll(".bttn");
+bttns.forEach((bttn)=>{
+    bttn.addEventListener('click',playRound);
+});
+
+const playAgainBttn = document.querySelector("#play-again");
+playAgainBttn.addEventListener("click",function(){
+    document.querySelector("#finalResult-container").style.visibility = "hidden";
+    document.querySelector("#player-score").textContent = "0";
+    document.querySelector("#computer-score").textContent = "0";
+    computerChoiceDiv.removeChild(computerChoiceDiv.firstChild);
+    playerChoiceDiv.removeChild(playerChoiceDiv.firstChild);
+    document.querySelector("#result").textContent = "Play Now";
+    document.querySelector("#play-field").style.pointerEvents ="auto";
+});
+
+
